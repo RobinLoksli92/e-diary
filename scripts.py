706d7790ad3f7ca6340e1222eba5dee5ef4880c9
RobinLoksli92 <gleb11tiun@mail.ru>
2022-02-11 15:51:16 +0200

    get() вместо filter() Изменено название аргумента schoolkid => kid Новая ф-я, которая достает карточку ученика Проверка на то, что урок вводимый пользователем существует

--- scripts.py
+++ scripts.py
@@ -4,21 +4,26 @@ import random
 from datacenter.models import Mark, Chastisement, Lesson, Schoolkid, Commendation
 
 
-def fix_marks(schoolkid):
-    schoolkid = Schoolkid.objects.filter(full_name__contains=schoolkid)[0]
+def get_kid_blank(kid):
+    schoolkid = Schoolkid.objects.get(full_name__contains=kid)
+    return schoolkid
+
+
+def fix_marks(kid):
+    schoolkid = get_kid_blank(kid)
     child_marks = Mark.objects.filter(schoolkid=schoolkid, points__in=[2,3])
     for child_mark in child_marks:
         child_mark.points = 5
         child_mark.save()
 
 
-def remove_chastisements(schoolkid):
-    schoolkid = Schoolkid.objects.filter(full_name__contains=schoolkid)[0]
+def remove_chastisements(kid):
+    schoolkid = get_kid_blank(kid)
     child_chastisements = Chastisement.objects.filter(schoolkid=schoolkid)
     child_chastisements.delete()
 
 
-def create_commendation(schoolkid, subject_title):
+def create_commendation(kid, subject_title):
     texts = ['Молодец!',
         'Отлично!',
         'Хорошо!',
@@ -49,15 +54,16 @@ def create_commendation(schoolkid, subject_title):
         'Ты растешь над собой!',
         'Ты многое сделал, я это вижу!',
         'Теперь у тебя точно все получится!']
-    schoolkid = Schoolkid.objects.filter(full_name__contains=schoolkid)[0]
+    schoolkid = get_kid_blank(kid)
     kid_study_year = schoolkid.year_of_study
     kid_group_letter = schoolkid.group_letter
     kid_lessons = Lesson.objects.filter(year_of_study=kid_study_year, group_letter=kid_group_letter)
-    certain_kid_lesson = kid_lessons.filter(subject__title=subject_title).order_by('?').first()
-    Commendation.objects.create(
-        schoolkid=schoolkid,
-        teacher=certain_kid_lesson.teacher, 
-        subject=certain_kid_lesson.subject,
-        created=certain_kid_lesson.date,
-        text=random.choice(texts)
-        )
+    if kid_lessons.filter(subject__title=subject_title).first() in kid_lessons:
+        certain_kid_lesson = kid_lessons.filter(subject__title=subject_title).order_by('?').first()
+        Commendation.objects.create(
+            schoolkid=schoolkid,
+            teacher=certain_kid_lesson.teacher, 
+            subject=certain_kid_lesson.subject,
+            created=certain_kid_lesson.date,
+            text=random.choice(texts)
+            )
